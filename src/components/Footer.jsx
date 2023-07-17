69b3d4c6e9593e0b16784fcb9c9c0fb25435a830
Syed Wajahat Ali <wajahat.klk25.com>
2023-07-17 21:29:46 +0100

    adds all the css styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <h3 className="f-text">Made by Syed Wajahat</h3>
+    </div>
+  );
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function home() {
+  return (
+    <div className="main">
+      <h1 className="m--heading">PALOMA</h1>
+      <ul className="nav">
+        <li>HEAR</li>
+        <li>LATEST</li>
+        <li>TOUR</li>
+        <li>SEE</li>
+        <li>SHOP</li>
+        <li>WATCH</li>
+        <li>PERSONAL</li>
+        <li>AID</li>
+      </ul>
+    </div>
+  );
+}
